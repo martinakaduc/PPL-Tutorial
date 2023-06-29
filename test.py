2dfa098ac570973d5370e7b857328b646cd7d247
Martin Nguyen <nguyenquangduc2000@gmail.com>
2023-06-29 22:35:49 +0700

    Factorized

---
+++ test.py
@@ -0,0 +1,28 @@
+import os
+
+cf = os.path.abspath(__file__)
+td = os.path.dirname(cf)
+print(td)
+
+modules = os.listdir(td)
+modules = [module for module in modules if os.path.isdir(
+    os.path.join(td, module))]
+
+for module in modules:
+    print("Testing", module)
+    examples = os.listdir(os.path.join(td, module))
+    for example in examples:
+        if 'Example' not in example:
+            continue
+
+        os.system(
+            f'''
+            cd "{td}/{module}/{example}/LanguageGenerator" && python gen.py BKIT.g4
+            '''
+        )
+
+        os.system(
+            f'''
+            cd "{td}/{module}/{example}/FinalProgram" && python main.py testcase.txt
+            '''
+        )
